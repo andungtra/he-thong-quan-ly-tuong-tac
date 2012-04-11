b8e8255aa0ee77e743a103e5c03d0d58f9ff69c0
hoang.dinh21 <hoang.dinh21@gmail.com>
2012-04-11 15:27:07 +0000

    update UI

--- SourceCode/src/main/java/org/hcmus/tis/controller/ProjectController.java
+++ SourceCode/src/main/java/org/hcmus/tis/controller/ProjectController.java
@@ -4,8 +4,14 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 import java.util.Set;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.validation.Valid;
+
 import org.hcmus.tis.model.MemberInformation;
 import org.hcmus.tis.model.Project;
+import org.hcmus.tis.model.StudyClass;
+import org.hcmus.tis.model.WorkItemContainer;
 import org.hcmus.tis.util.Parameter;
 import org.springframework.roo.addon.web.mvc.controller.finder.RooWebFinder;
 import org.springframework.roo.addon.web.mvc.controller.scaffold.RooWebScaffold;
@@ -15,6 +21,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.validation.BindingResult;
 
 @RequestMapping("/projects")
 @Controller
@@ -22,73 +29,104 @@ import org.springframework.web.bind.annotation.RequestParam;
 @RooWebFinder
 public class ProjectController {
 
-    @RequestMapping(value = "/{id}", produces = "text/html")
-    public String show(@PathVariable("id") Long id, Model uiModel) {
-        uiModel.addAttribute("itemId", id);
-        return "projects/show";
-    }
+	@RequestMapping(method = RequestMethod.POST, produces = "text/html")
+	public String create(@Valid Project project, BindingResult bindingResult,
+			Model uiModel, HttpServletRequest httpServletRequest) {
+		if (bindingResult.hasErrors()) {
+			populateEditForm(uiModel, project);
+			return "projects/create";
+		}
+		uiModel.asMap().clear();
+		project.persist();
+		// return "redirect:/projects/" +
+		// encodeUrlPathSegment(project.getId().toString(), httpServletRequest);
+		uiModel.addAttribute("projects", Project.findAllProjects());
+		return "redirect:/projects/list";
+	}
 
-    @RequestMapping(params = "find=quickFind", method = RequestMethod.GET)
-    public String findProjectsQuickly(@RequestParam("query") String name, Model uiModel, @RequestParam(value = "page", required = false) Integer page, @RequestParam(value = "size", required = false) Integer size) {
-        int sizeNo = size == null ? 10 : size.intValue();
-        int firstResult = page == null ? 0 : (page.intValue() - 1) * sizeNo;
-        long totalResult = 0;
-        if (!name.isEmpty()) {
-            uiModel.addAttribute("projects", Project.findProjectsByNameLike(name, firstResult, sizeNo).getResultList());
-            totalResult = Project.countProjectsByNameLike(name);
-        } else {
-            uiModel.addAttribute("projects", Project.findProjectEntries(firstResult, sizeNo));
-            totalResult = Project.countProjects();
-        }
-        Collection<Parameter> parameters = new ArrayList<Parameter>();
-        parameters.add(new Parameter("find", "quickFind"));
-        parameters.add(new Parameter("query", name));
-        uiModel.addAttribute("parameters", parameters);
-        int maxPage = (int) (totalResult / sizeNo);
-        if (totalResult % sizeNo != 0) {
-            ++maxPage;
-        }
-        uiModel.addAttribute("maxPages", maxPage);
-        uiModel.addAttribute("query", name);
-        return "projects/list";
-    }
+	void populateEditForm(Model uiModel, Project project) {
+		uiModel.addAttribute("project", project);
+		uiModel.addAttribute("memberinformations",
+				MemberInformation.findAllMemberInformations());
+		uiModel.addAttribute("studyclasses", StudyClass.findAllStudyClasses());
+		uiModel.addAttribute("workitemcontainers",
+				WorkItemContainer.findAllWorkItemContainers());
+	}
 
-    public String findProjectsByNameLike(String name, Model uiModel) {
-        return "projects/list";
-    }
+	@RequestMapping(value = "/{id}", produces = "text/html")
+	public String show(@PathVariable("id") Long id, Model uiModel) {
+		uiModel.addAttribute("itemId", id);
+		return "projects/show";
+	}
 
-    @RequestMapping(value = "/{id}/overview", produces = "text/html")
-    public String overview(@PathVariable("id") Long id, Model uiModel) {
-        uiModel.addAttribute("project", Project.findProject(id));
-        uiModel.addAttribute("itemId", id);
-        return "projects/overview";
-    }
+	@RequestMapping(params = "find=quickFind", method = RequestMethod.GET)
+	public String findProjectsQuickly(@RequestParam("query") String name,
+			Model uiModel,
+			@RequestParam(value = "page", required = false) Integer page,
+			@RequestParam(value = "size", required = false) Integer size) {
+		int sizeNo = size == null ? 10 : size.intValue();
+		int firstResult = page == null ? 0 : (page.intValue() - 1) * sizeNo;
+		long totalResult = 0;
+		if (!name.isEmpty()) {
+			uiModel.addAttribute("projects",
+					Project.findProjectsByNameLike(name, firstResult, sizeNo)
+							.getResultList());
+			totalResult = Project.countProjectsByNameLike(name);
+		} else {
+			uiModel.addAttribute("projects",
+					Project.findProjectEntries(firstResult, sizeNo));
+			totalResult = Project.countProjects();
+		}
+		Collection<Parameter> parameters = new ArrayList<Parameter>();
+		parameters.add(new Parameter("find", "quickFind"));
+		parameters.add(new Parameter("query", name));
+		uiModel.addAttribute("parameters", parameters);
+		int maxPage = (int) (totalResult / sizeNo);
+		if (totalResult % sizeNo != 0) {
+			++maxPage;
+		}
+		uiModel.addAttribute("maxPages", maxPage);
+		uiModel.addAttribute("query", name);
+		return "projects/list";
+	}
 
-    @RequestMapping(value = "/{id}/task", produces = "text/html")
-    public String task(@PathVariable("id") Long id, Model uiModel) {
-        uiModel.addAttribute("project", Project.findProject(id));
-        uiModel.addAttribute("itemId", id);
-        return "projects/task";
-    }
+	public String findProjectsByNameLike(String name, Model uiModel) {
+		return "projects/list";
+	}
 
-    @RequestMapping(value = "/{id}/wiki", produces = "text/html")
-    public String wiki(@PathVariable("id") Long id, Model uiModel) {
-        uiModel.addAttribute("project", Project.findProject(id));
-        uiModel.addAttribute("itemId", id);
-        return "projects/wiki";
-    }
+	@RequestMapping(value = "/{id}/overview", produces = "text/html")
+	public String overview(@PathVariable("id") Long id, Model uiModel) {
+		uiModel.addAttribute("project", Project.findProject(id));
+		uiModel.addAttribute("itemId", id);
+		return "projects/overview";
+	}
 
-    @RequestMapping(value = "/{id}/members", produces = "text/html")
-    public String listMembers(@PathVariable("id") Long id, Model uiModel) {
-        Set<MemberInformation> memberInformations = Project.findProject(id).getMemberInformations();
-        uiModel.addAttribute("memberinformations", memberInformations);
-        return "projects/member";
-    }
+	@RequestMapping(value = "/{id}/task", produces = "text/html")
+	public String task(@PathVariable("id") Long id, Model uiModel) {
+		uiModel.addAttribute("project", Project.findProject(id));
+		uiModel.addAttribute("itemId", id);
+		return "projects/task";
+	}
 
-    @RequestMapping(value = "/{id}/calendar", produces = "text/html")
-    public String calendar(@PathVariable("id") Long id, Model uiModel) {
-        uiModel.addAttribute("project", Project.findProject(id));
-        uiModel.addAttribute("itemId", id);
-        return "projects/calendar";
-    }
+	@RequestMapping(value = "/{id}/wiki", produces = "text/html")
+	public String wiki(@PathVariable("id") Long id, Model uiModel) {
+		uiModel.addAttribute("project", Project.findProject(id));
+		uiModel.addAttribute("itemId", id);
+		return "projects/wiki";
+	}
+
+	@RequestMapping(value = "/{id}/members", produces = "text/html")
+	public String listMembers(@PathVariable("id") Long id, Model uiModel) {
+		Set<MemberInformation> memberInformations = Project.findProject(id)
+				.getMemberInformations();
+		uiModel.addAttribute("memberinformations", memberInformations);
+		return "projects/member";
+	}
+
+	@RequestMapping(value = "/{id}/calendar", produces = "text/html")
+	public String calendar(@PathVariable("id") Long id, Model uiModel) {
+		uiModel.addAttribute("project", Project.findProject(id));
+		uiModel.addAttribute("itemId", id);
+		return "projects/calendar";
+	}
 }
