dbf3029726dbd0fe8bdb5650c9a660347103f82b
abcxyz2357@gmail.com <abcxyz2357@gmail.com@b07b22ed-11c3-a3b6-008f-70c30f1c32eb>
2012-07-01 15:21:22 +0000

    fix issue #76 : can't delete project's member.

--- trunk/SourceCode/src/main/java/org/hcmus/tis/repository/WorkItemRepository.java
+++ trunk/SourceCode/src/main/java/org/hcmus/tis/repository/WorkItemRepository.java
@@ -4,12 +4,12 @@ import java.util.List;
 
 import org.hcmus.tis.model.Account;
 import org.hcmus.tis.model.WorkItem;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.roo.addon.layers.repository.jpa.RooJpaRepository;
 
 @RooJpaRepository(domainType = WorkItem.class)
 public interface WorkItemRepository extends WorkItemRepositoryCustom {
-	@Query("SELECT o FROM WorkItem o  WHERE o.asignee.account =?1 AND o.status.closed =?2")
-	public List<WorkItem> findByAsigneeAndFinalStatus(Account account, boolean deleted);
+	public List<WorkItem> findByAsignee_AccountAndStatus_Closed(Account account, boolean closed, Pageable pageable);
 	
 }
